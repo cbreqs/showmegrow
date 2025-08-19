c6f6e323fbb045ca3a3ea04789f7f67635ebdd63
cbreqs <singularity@reqs.tech>
2025-08-19 10:02:27 -0500

    Update firebase-config.js
    firebase code update to correct app

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,21 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
-try {
-   const config = {
-    apiKey: "AIzaSyB9TwtGcDQJrJgYlcpUmtggJxvcF6MTlCk",
-    authDomain: "showmegrowinfo.firebaseapp.com",
-    databaseURL: "https://showmegrowinfo-default-rtdb.firebaseio.com",
-    projectId: "showmegrowinfo",
-    storageBucket: "showmegrowinfo.firebasestorage.app",
-    messagingSenderId: "317414799927",
-    appId: "1:317414799927:web:81a6a76b52511bbad51db2"
-  };
-  if (!Object.values(config).some(v => String(v).startsWith('YOUR_'))) {
-    firebaseApp = firebase.initializeApp(config);
-    db = firebase.firestore();
-    isConfigured = true;
-  }
-} catch (e) {
-  console.warn('Firebase not available until deployed with config.', e);
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyB9TwtGcDQJrJgYlcpUmtggJxvcF6MTlCk",
+  authDomain: "showmegrowinfo.firebaseapp.com",
+  projectId: "showmegrowinfo",
+  // storageBucket is fine to leave in, but Firestore doesn't use it.
+  storageBucket: "showmegrowinfo.appspot.com"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
+
