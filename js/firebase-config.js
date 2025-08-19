2182d0672a3928d30f3a7741f95aa9f0bbc801a3
cbreqs <singularity@reqs.tech>
2025-08-19 10:15:13 -0500

    Update firebase-config.js
    corrected firebase code, i hope

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,13 @@
-...
+// js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9TwtGcDQJrJgYlcpUmtggJxvcF6MTlCk",
+  authDomain: "showmegrowinfo.firebaseapp.com",
+  projectId: "showmegrowinfo",
+  storageBucket: "showmegrowinfo.appspot.com"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
