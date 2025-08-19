62ab91ea4c35b2df48af3f4bc8e64c339f952ed2
cbreqs <singularity@reqs.tech>
2025-08-19 10:08:16 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,1 @@
-// js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB9TwtGcDQJrJgYlcpUmtggJxvcF6MTlCk",
-  authDomain: "showmegrowinfo.firebaseapp.com",
-  projectId: "showmegrowinfo",
-  // storageBucket is fine to leave in, but Firestore doesn't use it.
-  storageBucket: "showmegrowinfo.appspot.com"
-};
-
-export const firebaseApp = initializeApp(firebaseConfig);
-export const db = getFirestore(firebaseApp);
-
+...
